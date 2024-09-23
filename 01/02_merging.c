4bd00924de6942aaf85373c0833c1be4ec139414
Basu Slathia <basuslathia01@gmail.com>
2024-09-23 23:00:45 +0530

    Create 02_merging.c

---
+++ 01/02_merging.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main(){
+
+    int a[10],b[10],c[20];
+    int i,j,k,n1,n2;
+    printf("how many elements of a ");
+    scanf("%d",&n1);
+    printf("enter %d elements",n1);
+    for(i=0;i<n1;i++)
+    scanf("%d",&a[i]);
+    printf("how many elemetns of b");
+    scanf("%d",&n2);
+    printf("enter %d elemetns ",n2);
+    for(i=0;i<n2;i++)
+    scanf("%d",&b[i]);
+    
+}
